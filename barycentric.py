a141a5a484ad6365127bf6f530a9dfed0d971f35
pkulkarnithegator <pp.kulkarni@ufl.edu>
2025-02-21 20:17:46 -0500

    Add files via upload

--- barycentric.py
+++ barycentric.py
@@ -1,29 +1,34 @@
+from matplotlib.patches import Polygon
+import matplotlib.pyplot as plt
 import numpy as np
 
-def get_barycentric_coordinates(triangle_coordinates: ndarray, point_coordinates: np.ndarray) -> ndarray:
-    system_array = np.array(
+import barycentric
+
+import numpy as np
+def get_barycentric_coordinates(triangle_coordinates, point_coordinates):
+    left_side = np.array([[point_coordinates[0]], [point_coordinates[1]], [1]])
+    right_side = np.array(
         [
-            [triangle_coordinates[0, 0], triangle_coordinates[0, 1], triangle_coordinates[0, 2]],
-            [triangle_coordinates[1, 0], triangle_coordinates[1, 1], triangle_coordinates[1, 2]],
+            [triangle_coordinates[0][0], triangle_coordinates[0][1], triangle_coordinates[0][2]],
+            [triangle_coordinates[1][0], triangle_coordinates[1][1], triangle_coordinates[1][2]],
             [1, 1, 1]
         ]
     )
+    return np.linalg.solve(right_side, left_side).tolist() 
 
-    point_array = np.array([point_coordinates[0], point_coordinates[1], point_coordinates[0]])
 
-    lambdas_array = np.linalg.solve(system_array, point_array)
+def get_cartesian_coordinates(triangle_coordinates, barycentric_coordinates):
+    the_x = (triangle_coordinates[0][0] * (barycentric_coordinates[0][0]) +
+         triangle_coordinates[0][1] * float(barycentric_coordinates[1][0]) +
+         triangle_coordinates[0][2] * float(barycentric_coordinates[2][0]))
+    
+    the_y = (triangle_coordinates[1][0] * barycentric_coordinates[0][0] +
+         triangle_coordinates[1][1] * barycentric_coordinates[1][0] +
+         triangle_coordinates[1][2] * barycentric_coordinates[2][0])
+    
+    return [the_x, the_y]
 
-    return lambdas_array
+def is_inside_triangle(triangle_coordinates, point_coordinates):
+    the_coords = get_barycentric_coordinates(triangle_coordinates, point_coordinates)
+    return the_coords[0][0] >= 0 and the_coords[1][0] >= 0 and the_coords[2][0] >= 0
 
-def get_cartesian_coordinates(triangle_coordinates: ndarray, barycentric_coordinates: ndarray) -> ndarray:
-    x = np.dot(triangle_coordinates[0:], barycentric_coordinates)
-    y = np.dot(triangle_coordinates[1:], barycentric_coordinates)
-    return np.array([x, y])
-
-def is_inside_triangle(triangle_coordinates: ndarray, point_coordinates: np.ndarray) -> bool:
-    lams = get_barycentric_coordinates(triangle_coordinates, point_coordinates)
-
-    if  lams[0] > 0 and lams[1] > 0 and lams[2] > 0:
-        return True
-    else:
-        return False
